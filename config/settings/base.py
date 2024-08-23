49babbc282221ce55dda23cb532f40e72526f35f
seeedata <lse021004@naver.com>
2024-08-24 07:01:36 +0900

    settings.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = []
+'''
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+'''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
